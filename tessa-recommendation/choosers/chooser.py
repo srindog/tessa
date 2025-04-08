9f531002ba0169f3b5e6a735985c4e29f81a1cf6
Sandhya Murthy Laliwala <sandhyamurthylaliwala@Sandhyas-MacBook-Pro.local>
2025-04-08 14:19:21 -0400

    clean directory

--- tessa-recommendation/choosers/chooser.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Chooser(ABC):
-
-  @abstractmethod
-  def choose(self):
-    pass
--- tessa-recommendation/templates/invites.py
+++
@@ -1,1 +0,0 @@
-NETWORKING_INVITE_TEMPLATE = """Hi {}, hope you are doing well! I am currently a {} at {}. Your career journey is so inspiring and I would love to learn more about your experiences as a {} in {}. If you have a few minutes, would love to connect. Thanks!"""
